46cc5c451f8d27de1a95e003fb2b64bde38c3481
gary87004 <r03942098@ntu.edu.tw>
2016-11-02 00:00:19 +0800

    Create arrangeCoins.c

---
+++ arrangeCoins.c
@@ -0,0 +1,17 @@
+int arrangeCoins(int n) {
+    int row_num = 1;
+    if(n==1)
+        return 1;
+    while(n - row_num > 0)
+        {
+            n = n - row_num;
+            row_num++;
+            
+            
+        }
+    if(n - row_num < 0)
+        return row_num-1;
+    else
+        return row_num;
+    
+}
